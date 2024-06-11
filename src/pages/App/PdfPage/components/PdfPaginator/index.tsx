7e8fff1305979144fb2eb9c7b5c4f208cc3ccf18
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-06-11 16:59:47 -0300

    implementado parcialmente tela de pdf

--- src/pages/App/PdfPage/components/PdfPaginator/index.tsx
+++ src/pages/App/PdfPage/components/PdfPaginator/index.tsx
@@ -1,17 +1,37 @@
 import styles from './styles.module.scss';
+import ChevronTop from '../../../../../assets/svgs/pdfPaginator/pdf_paginator_chevron_top.svg';
+import ChevronDown from '../../../../../assets/svgs/pdfPaginator/pdf_paginator_chevron_down.svg';
 
-export function PdfPaginator() {
+interface PdfPaginatorProps {
+    currentPage: number,
+    pages: number,
+    handlePage: (value: "up" | "down") => void
+}
+
+export function PdfPaginator({ currentPage, pages, handlePage }: PdfPaginatorProps) {
     return (
         <article className={styles.card}>
             <div className={styles.page_handler}>
-                <button>C</button>
-                <button>B</button>
+                <button
+                    onClick={
+                        () => { handlePage("up") }
+                    }>
+                    <img src={ChevronTop} alt="" />
+                </button>
+                <button
+                    onClick={
+                        () => { handlePage("down") }
+                    }>
+                    <img src={ChevronDown} alt="" />
+                </button>
             </div>
             <div className={styles.divisor}>
 
             </div>
             <div className={styles.page_count}>
-
+                <span>{currentPage}</span>
+                <span>/</span>
+                <span>{pages}</span>
             </div>
         </article>
     )
--- src/pages/App/PdfPage/index.tsx
+++ src/pages/App/PdfPage/index.tsx
@@ -4,23 +4,34 @@ import { Document, Page } from 'react-pdf'
 import { useEffect, useState } from 'react';
 import { getBookById } from '../../../services/http/conteudos/livros';
 import { toast } from 'react-toastify';
-
 import { pdfjs } from 'react-pdf';
-//import "../../../../node_modules/pdfjs-dist/build/pdf.worker.min.mjs"
+import { PdfPaginator } from './components/PdfPaginator';
+import { Book } from '../../../services/http/conteudos/livros/types';
+import ChevronLeftIcon from "../../../assets/svgs/chevron_left_pdf_scren.svg";
+import MenuIcon from "../../../assets/svgs/menu_icon_pdf_page.svg";
+import DownloadIcon from "../../../assets/svgs/pdfMenu/download.svg";
+import PrintIcon from "../../../assets/svgs/pdfMenu/print.svg";
+import ShareIcon from "../../../assets/svgs/pdfMenu/share.svg";
+import copy from 'copy-to-clipboard';
+
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     "../../../../node_modules/pdfjs-dist/build/pdf.worker.min.mjs",
     import.meta.url,
 ).toString();
 
-
 export function PdfPage() {
     const params = useParams();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [pdfUrl, setPdfUrl] = useState("");
-    const [numPages, setNumPages] = useState<number>();
-    const [pageNumber, setPageNumber] = useState<number>(1);
+    const [numPages, setNumPages] = useState<number>(0);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [book, setBook] = useState<Book>({
+        id: "",
+        arquivo: "",
+        titulo: ""
+    } as Book);
 
     useEffect(() => {
-
         fetchPdf();
     }, []);
 
@@ -33,6 +44,7 @@ export function PdfPage() {
 
         try {
             const { data } = await getBookById(id);
+            setBook(data.data);
             setPdfUrl(data.data.arquivo);
         } catch (error) {
             toast.error("Erro ao carregar pdf")
@@ -43,45 +55,24 @@ export function PdfPage() {
         setNumPages(numPages);
     }
 
-    async function fetchPdfAsBlob(url: string): Promise<Blob> {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+    function handlePage(value: "up" | "down") {
+        if (value == "up") {
+            return pageUp();
         }
 
-        // Obter o leitor de stream da resposta
-        const reader = response.body?.getReader();
-        if (!reader) {
-            throw new Error('Failed to get reader from response body');
+        return pageDown();
+    }
+
+    function pageUp() {
+        if (numPages > currentPage) {
+            setCurrentPage(currentPage + 1);
         }
+    }
 
-        // Armazenar os pedaços de dados do PDF
-        const chunks: Uint8Array[] = [];
-        let receivedLength = 0;
-
-        // Ler os dados da resposta
-        while (true) {
-            const { done, value } = await reader.read();
-            if (done) break;
-            if (value) {
-                chunks.push(value);
-                receivedLength += value.length;
-            }
+    function pageDown() {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
         }
-
-        // Concatena todos os pedaços de dados
-        const pdfArray = new Uint8Array(receivedLength);
-        let position = 0;
-        for (const chunk of chunks) {
-            pdfArray.set(chunk, position);
-            position += chunk.length;
-        }
-
-        // Cria um Blob a partir do Uint8Array
-        const blob = new Blob([pdfArray], { type: 'application/pdf' });
-
-        return blob;
     }
 
     function renderPdf(link: string) {
@@ -93,33 +84,83 @@ export function PdfPage() {
             <>
                 <div className={styles.pdf_container}>
                     <Document
-
                         className={styles.pdf_file}
                         file={link}
                         onLoadSuccess={onDocumentLoadSuccess}
-                    // options={{ workerSrc: `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js` }}
-
+                        loading={"Carregando pdf"}
+                        error={"Erro ao carregar o pdf"}
                     >
-                        <Page pageNumber={pageNumber} scale={2} loading={"Carregando pdf"} error={"Erro ao carregar o pdf"} />
+                        <Page
+                            renderAnnotationLayer={false}
+                            renderTextLayer={false}
+                            pageNumber={currentPage}
+                            scale={1}
+                            loading={"Carregando pdf"}
+                            error={"Erro ao carregar o pdf"}
+                        />
                     </Document>
 
-                    <p>
-                        Page {pageNumber} of {numPages}
-                    </p>
+                    <div className={styles.paginator_container}>
+                        <PdfPaginator
+                            pages={numPages}
+                            currentPage={currentPage}
+                            handlePage={handlePage}
+                        />
+                    </div>
                 </div>
             </>
         )
     }
 
+    function renderMenu(render: boolean) {
+        if (render) {
+            return (
+                <ul className={styles.header_menu}>
+                    <li>
+                        <div className={styles.icon_box}>
+                            <img src={DownloadIcon} alt="" />
+                        </div>
+                        <span>Fazer download</span>
+                    </li>
+                    <li onClick={copyLink}>
+                        <div className={styles.icon_box}>
+                            <img src={ShareIcon} alt="" />
+                        </div>
+                        <span>Copiar link</span>
+                    </li>
+                    <li>
+                        <div className={styles.icon_box}>
+                            <img src={PrintIcon} alt="" />
+                        </div>
+                        <span>Imprimir</span>
+                    </li>
+
+                </ul>
+            )
+        }
+
+        return null;
+    }
+
+    function copyLink() {
+        copy(pdfUrl);
+        toast.success("Link copiado");
+        setIsMenuOpen(false);
+    }
+
     return (
         <div className={styles.main}>
             <header className={styles.header}>
-                <div className={styles.header_conteiner}>
-                    <button>{"<"}</button>
-
-                    <button>{"..."}</button>
+                <div className={styles.header_container}>
+                    <button className={styles.action_btn} aria-label='voltar'>
+                        <img src={ChevronLeftIcon} alt="" />
+                    </button>
+                    <title>{book.titulo}</title>
+                    <button className={`${styles.action_btn} ${styles.back_button}`} aria-label='menu' onClick={() => { setIsMenuOpen(!isMenuOpen) }}>
+                        <img src={MenuIcon} alt="" />
+                    </button>
+                    {renderMenu(isMenuOpen)}
                 </div>
-
             </header>
             {renderPdf(pdfUrl)}
         </div>
